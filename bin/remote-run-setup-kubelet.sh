fd71f8e8129ac5851122a83ad02f32780cd4cf80
Dr Nic Williams <drnicwilliams@gmail.com>
2019-11-08 12:41:42 +1000

    initial gcloud to create networking and VMs

---
+++ bin/remote-run-setup-kubelet.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eu
+
+MASTERS=${MASTERS:-1}
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+i=0
+while [ $i -lt $MASTERS ]; do
+  instance=controller-$i
+  (
+  set -x
+  gcloud compute scp \
+    $DIR/setup-kubelet.sh \
+    ${instance}:~/
+
+  gcloud compute ssh ${instance} -- bash setup-kubelet.sh
+
+  )
+  ((i++))
+done
