6396bb874d2782ddb6df245e0fcb37489fb41262
Dr Nic Williams <drnicwilliams@gmail.com>
2019-11-12 09:59:08 +1000

    setup nginx /healthz on each controller

--- bin/setup-from-debian.sh
+++ bin/setup-from-debian.sh
@@ -99,6 +99,25 @@ if [[ "$HOSTNAME" == "controller-0" ]]; then
 
   dig $HOSTNAME +short | head -n1 > controller-0-ip
   )
+
+  echo "Setup nginx for HTTPS /healthz checks"
+  apt-get install nginx -y
+
+  cat > /etc/nginx/sites-available/kubernetes.default.svc.cluster.local <<EOF
+server {
+  listen      80;
+  server_name kubernetes.default.svc.cluster.local;
+
+  location /healthz {
+     proxy_pass                    https://127.0.0.1:6443/healthz;
+     proxy_ssl_trusted_certificate /var/lib/kubernetes/ca.pem;
+  }
+}
+EOF
+  ln -s /etc/nginx/sites-available/kubernetes.default.svc.cluster.local /etc/nginx/sites-enabled/ || echo skipping...
+
+  systemctl restart nginx
+  systemctl enable nginx
 else
   (
   set -x
